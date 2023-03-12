29258e2be97e12cf163c5d1ec943c86dae28beaa
jacobRidenour <jacob.ridenour@live.com>
2023-03-12 17:57:19 -0400

    Added VS cpp project with more learning/basics

---
+++ C++/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //<> = in standard library
+
+int main() {
+    std::cout << "Hello World" << std::endl; //cout = character out [to the screen]
+    return 0; //not strictly necessary but good to have
+}
+
+// << write data to a stream
+// >> read data from a stream
